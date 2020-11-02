a790cf622f61e41fd3973f661bb78eefaf386f2f
Yuta Asato <yuta@Yutas-MacBook-Pro.local>
2020-11-02 12:28:41 +0900

    db heroku

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
