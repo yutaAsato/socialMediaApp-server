0c02824cc2285dc52150130e97df50ba02dc03bc
Yuta Asato <yuta@Yutas-MacBook-Pro.local>
2021-09-05 16:38:50 +0900

    u

--- admin.js
+++ admin.js
@@ -8,18 +8,23 @@ let db;
 
 console.log("env", process.env.NODE_ENV);
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  console.log("development mode");
+// pool = new Pool({
+//   connectionString:
+//     "postgres://wdjjyskcleyolm:31ec7208768c4f238525c5ebc7f8ab45d154ca22dfd5f5400f58702bb9b95a70@ec2-54-236-137-173.compute-1.amazonaws.com:5432/d9i94tomfsecqi",
+// });
 
-  // db = knex({
-  //   client: "pg",
-  //   connection: {
-  //     host: "127.0.0.1",
-  //     user: "yuta",
-  //     password: process.env.PASSWORD,
-  //     database: "socialmedia",
-  //   },
-  // });
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // console.log("development mode");
+
+  // // db = knex({
+  // //   client: "pg",
+  // //   connection: {
+  // //     host: "127.0.0.1",
+  // //     user: "yuta",
+  // //     password: process.env.PASSWORD,
+  // //     database: "socialmedia",
+  // //   },
+  // // });
 
   pool = new Pool({
     host: "127.0.0.1",
@@ -28,13 +33,13 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     port: 5432,
     database: "socialmedia",
   });
+
+  console.log("production");
 } else {
   console.log("production");
 
   pool = new Pool({
-    // connectionString: process.env.DATABASE_URL,
-    connectionString:
-      "postgres://wdjjyskcleyolm:31ec7208768c4f238525c5ebc7f8ab45d154ca22dfd5f5400f58702bb9b95a70@ec2-54-236-137-173.compute-1.amazonaws.com:5432/d9i94tomfsecqi",
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   });
 }
