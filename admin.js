4529eec678b0f543b984c66caed10e7e50c1bfe0
Yuta Asato <yuta@Yutas-MacBook-Pro.local>
2021-09-05 21:23:39 +0900

    u

--- admin.js
+++ admin.js
@@ -10,46 +10,50 @@ let db;
 
 console.log("env", process.env.NODE_ENV);
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  // // db = knex({
-  // //   client: "pg",
-  // //   connection: {
-  // //     host: "127.0.0.1",
-  // //     user: "yuta",
-  // //     password: process.env.PASSWORD,
-  // //     database: "socialmedia",
-  // //   },
-  // // });
+client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
 
-  // pool = new Pool({
-  //   host: "127.0.0.1",
-  //   user: "postgres",
-  //   // password: process.env.PASSWORD,
-  //   password: process.env.PASSWORD,
-  //   port: 5432,
-  //   database: "socialmedia",
-  // });
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   // // db = knex({
+//   // //   client: "pg",
+//   // //   connection: {
+//   // //     host: "127.0.0.1",
+//   // //     user: "yuta",
+//   // //     password: process.env.PASSWORD,
+//   // //     database: "socialmedia",
+//   // //   },
+//   // // });
 
-  client = new Client({
-    host: "127.0.0.1",
-    user: "postgres",
-    // password: process.env.PASSWORD,
-    password: process.env.PASSWORD,
-    port: 5432,
-    database: "socialmedia",
-  });
-} else {
-  client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+//   // pool = new Pool({
+//   //   host: "127.0.0.1",
+//   //   user: "postgres",
+//   //   // password: process.env.PASSWORD,
+//   //   password: process.env.PASSWORD,
+//   //   port: 5432,
+//   //   database: "socialmedia",
+//   // });
 
-  // pool = new Pool({
-  //   connectionString: process.env.DATABASE_URL,
-  // });
-}
+//   client = new Client({
+//     host: "127.0.0.1",
+//     user: "postgres",
+//     // password: process.env.PASSWORD,
+//     password: process.env.PASSWORD,
+//     port: 5432,
+//     database: "socialmedia",
+//   });
+// } else {
+//   client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+
+//   // pool = new Pool({
+//   //   connectionString: process.env.DATABASE_URL,
+//   // });
+// }
 client.connect();
 
 module.exports = { db, pool, client };
